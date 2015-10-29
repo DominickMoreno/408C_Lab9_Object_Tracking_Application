c88de13ac1d91412ee45a30d7845970f9392d651
Dominick Moreno <dmoreno1@fresnel.physics.umd.edu>
2015-10-29 15:31:11 -0400

    Made a test hotfix in hotfix-branch

--- test.c
+++ test.c
@@ -4,5 +4,7 @@ int main() {
 	printf("Hello!\n");
 	printf("Hello AGAIN!\n");
 	printf("Can I ask you something crazy?\n");
+	printf("No! This is the hotfixed master branch!");
+	printf(" This is no time for sandwiches!\n");
 	return 0;
 }
