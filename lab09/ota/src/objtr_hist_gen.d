12a21516211b31c966aa6f39ede80c833c7bd116
Jacob Knapo <jknapo@compute2.physics.umd.edu>
2015-11-05 17:49:53 -0500

    File Cleanup

--- lab09/ota/src/objtr_hist_gen.d
+++
@@ -1,9 +0,0 @@
-objtr_hist_gen.o objtr_hist_gen.d : objtr_hist_gen.c \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/edges/basic/lide_c_fifo.h \
- objtr_hist_gen.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/tools/runtime/lide_c_util.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h
