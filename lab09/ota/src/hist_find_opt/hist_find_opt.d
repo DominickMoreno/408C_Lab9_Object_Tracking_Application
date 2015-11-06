44522bcb22c0806da18e18ff17597fa448adf7bf
carpentierkim <carpentier.kim@gmail.com>
2015-11-05 22:44:24 -0500

    Added tests for objtr_hist_find_opt

--- lab09/ota/src/hist_find_opt/hist_find_opt.d
+++
@@ -1,9 +0,0 @@
-hist_find_opt.o hist_find_opt.d : hist_find_opt.c \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/edges/basic/lide_c_fifo.h \
- hist_find_opt.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/tools/runtime/lide_c_util.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h
--- lab09/ota/src/lide_c_objtr_hist_gen/src/lide_c_objtr_hist_gen.d
+++
@@ -1,9 +0,0 @@
-lide_c_objtr_hist_gen.o lide_c_objtr_hist_gen.d : lide_c_objtr_hist_gen.c \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/edges/basic/lide_c_fifo.h \
- lide_c_objtr_hist_gen.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/tools/runtime/lide_c_util.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_basic.h \
- /dept/enee/software/dspcad/idice/idice-set/packages/lide1.0/lide/lang/c/src/gems/actors/common/lide_c_actor_context_type_common.h
